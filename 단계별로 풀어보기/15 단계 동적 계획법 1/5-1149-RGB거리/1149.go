7dc6080cd3400be20a62b6e4d338772550247d50
49EHyeon42 <49EHyeon42@gmail.com>
2021-11-15 14:29:26 +0900

    Add 15 단계/5-1149

---
+++ 단계별로 풀어보기/15 단계 동적 계획법 1/5-1149-RGB거리/1149.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var N int
+	fmt.Fscanln(reader, &N)
+
+	dp := make([][]int, N)
+	for i := range dp {
+		dp[i] = make([]int, 3)
+	}
+
+	var R, G, B int
+	for i := 0; i < N; i++ {
+		fmt.Fscanln(reader, &R, &G, &B)
+		dp[i][0], dp[i][1], dp[i][2] = R, G, B
+	}
+	for i := 1; i < N; i++ {
+		dp[i][0] += min(dp[i-1][1], dp[i-1][2])
+		dp[i][1] += min(dp[i-1][0], dp[i-1][2])
+		dp[i][2] += min(dp[i-1][0], dp[i-1][1])
+	}
+	fmt.Fprint(writer, min(dp[N-1][0], dp[N-1][1], dp[N-1][2]))
+	writer.Flush()
+}
+
+func min(arr ...int) int {
+	min := 9223372036854775807
+	for _, value := range arr {
+		if min > value {
+			min = value
+		}
+	}
+	return min
+}
